dd889be78089e1fe723e74f158c3dd6bb6233c00
linzizhaogif <aa837610059@2980.com>
2016-08-11 23:56:30 +0800

    -A

---
+++ App/Controllers/index.php
@@ -0,0 +1,8 @@
+<?php
+   define('DIR_SECURE_FILENAME', false);
+    define('APP_PATH', './MyBlog/');
+    define('APP_DEBUG', TRUE);
+    // define('BIND_MODULE', 'Home');
+   include './ThinkPHP/ThinkPHP.php';
+
+?>
