871c7725a582fb5faf395948157ba334630e3117
DESKTOP-GD7G0BH\huizhe <a22783276@gmail.com>
2016-08-14 00:12:51 +0800

    update register  system

--- App/Controllers/Enroll.php
+++ App/Controllers/Enroll.php
@@ -9,44 +9,32 @@ namespace App\Controllers;
 
 use App\Models\User;
 use App\Lib\Document;
+use App\Lib\Mail;
+use App\Lib\Vcode;
 
  class Enroll
  {
-	/**获取注册数据
-	*@param string nickname		昵称
-	*@param string mail         邮箱
-	*@param string password     密码        
+	/**实现注册
+	*       
 	*@param string photo 		头像  默认为 avatar/head.gif
 	*
-	*return status.返回插入的id或状态/错误代码
+	*return status.返回插入的id
 	*/
-	public function Register($nickname,$mail,$password,$photo="avatar/head.gif")
-	{
-		$checkdata=0;
-		if(preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$mail))
-		{
-			$data=User::where('mail','=',$mail)->select('mail');
-			foreach($data as $value){
-				if(in_array($mail, $value)){
-					$checkdata=1;
-				}
-			}
-			if(1==$checkdata)
-			{
-				response(['status'=> -1,'msg' =>'邮箱已存在']);
-			}else{
-				$login_id=User::Insert([
-					"nickname" =>$nickname,
-					"mail" =>$mail,
-					"password"=>$password,
-					"photo" =>$photo,
-					"role" =>5
-					]);
-				response(['status'=>$login_id]);
-			}
-		}else{
-			response(['status'=>-2,'msg'=>'邮箱格式错误']);
-		}
+	public function Register($photo="avatar/head.gif")
+	{	
+		$nickname=cache::get("nickname");
+		$mail 	 =cache::get("mail");
+		$password=cache::get("password");
+		$password=password_hash($password,PASSWORD_BCRYPT,['cost'=>mt_rand(7,10)]);
+		$login_id=User::Insert([
+			"nickname" =>$nickname,
+			"mail" =>$mail,
+			"password"=>$password,
+			"photo" =>$photo,
+			"role" =>4
+			]);
+		cache::flush();
+		response(['status'=>$login_id]);		
 	}
 	
 	/**更换头像
@@ -65,5 +53,73 @@ use App\Lib\Document;
 		response(["status"=>$src]);	
 	}
 	
+	/**发送邮件
+	*
+	*@param string mail 	邮箱
+	*@param string title 	标题
+	*@param string content   发送内容
+	*@param string nickname		昵称
+	*@param string password     密码  
+	*
+	*return status.状态/错误 
+	*/
+	public function sendEmail($mail,$title,$content,$nickname,$password)
+	{
+		$checkdata=0;
+		if(preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$mail))
+		{
+			$data=User::where('mail','=',$mail)->select('mail');
+			foreach($data as $value){
+				if(in_array($mail, $value)){
+					$checkdata=1;
+				}
+			}
+			if(1==$checkdata)
+			{
+				response(['status'=> -1,'msg' =>'邮箱已存在']);
+			}else{				
+				cache::set([
+					"nickname"=>$nickname,
+					"mail"=>$mail,
+					"password"=>$password
+					Mail::to($mail)->title($title)->content($content);
+					response(['status'=>1,'msg'=>"已发送邮件"]);
+					]);
+		 		response("status");
+			}
+		}else{
+			response(['status'=>-2,'msg'=>'邮箱格式错误']);
+		}		 
+	}
 
+	/**获取验证码图片
+	*
+	*return status.
+	*/
+
+	public function Vphoto()
+	{		
+		$verify=new Vcode();	
+		$verify->show();
+		$Vdata=$verify->getData();
+		session::set("Vda",$Vdata["text"]);		
+	}
+
+	/**获取验证
+	*
+	*@param string Vcheckdata  输入的验证码
+	*
+	*return status.返回状态/错误
+	*/
+	public function CheckVerify($Vcheckdata)
+	{
+		$verify=session::get("Vda");
+		if($verify==$Vcheckdata)
+		{
+			response(['status'=>1,'msg'=>"验证码正确"]);
+		}else{
+			response(['status'=>-1,'msg'=>"验证码错误"]);
+		}
+		session::remove("Vda");
+	}
 }
