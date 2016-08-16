ce58fb50799c0c9ea7dd0f3bcfd9a73728ca0e1e
linzizhaogif <aa837610059@2980.com>
2016-08-16 23:41:04 +0800

    App/Controllers/Login.php

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Controllers;
-
+use  App\Lib\Vcode;
 class Login
 {
     /**登录接口
@@ -9,16 +9,20 @@ class Login
    * @return status.状态/错误代码，$data成员信息包括id,name,sex,photo,department,habbit,position,blog,phone,introduction
 
    */
-    public function CheckLogin($mail,$password)
+    public function CheckLogin($mail,$password,$code)
     {
-         $d=TB('user')->where('mail', '=', $mail)->select();
+        if($code==Session::get("Vda")&&Session::get("Vda")!=null)
+        {
+            Session::set("Vda",null); 
+            $d=TB('user')->where('mail', '=', $mail)->select();
             if(sizeof($d)!=0)
             {
-                 if(password_verify($password, $d[0]['password']))
+                if(password_verify($password, $d[0]['password']))
                 {
                  unset($d[0]['password']); 
                  Session::set("user", $d[0]);
-                  $status=["status" => "1",'msg' => "登录成功！"];
+                 $status=["status" => "1",'msg' => "登录成功！"];
+
                  response($status, "json");
 
                 }
@@ -33,5 +37,24 @@ class Login
                 $status=["status" => "0",'msg' => "邮箱不存在！"];
                 response($status, "json");
             }
+           
+        }
+        else if(Session::get("Vda")==null)
+        {
+            $status=["status" => "0",'msg' => "验证码过期请刷新"];
+            response($status, "json");  
+        }
+        else{
+            $status=["status" => "0",'msg' => "验证码错误！"];
+            Session::set("Vda",null); 
+            response($status, "json");
+        }
     }
+
+    public function Get_Vtype()
+    {
+        $data=Session::get("Vtype");     
+        response($data,'json');
+    }
+ 
 }
