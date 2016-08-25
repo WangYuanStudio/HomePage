24390e89d56b3cdd866bd997b9260ed5a20b9589
Zeffee <me@zeffee.com>
2016-08-25 21:44:15 +0800

    zizhao

--- App/Middles/check_login.php
+++ App/Middles/check_login.php
@@ -2,7 +2,7 @@
 namespace App\Middles;
 use App\Controllers\Session;
 use App\Lib\Response;
-class check_login implements MiddleWare
+class Check_login implements MiddleWare
 {
     public function before($request)
     {
