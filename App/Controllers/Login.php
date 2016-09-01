f2d22cca5f4a23179c5bd412ddc3e50adcc473f9
linzizhaogif <aa837610059@2980.com>
2016-09-02 00:26:44 +0800

    登陆修改

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -3,10 +3,13 @@ namespace App\Controllers;
 use  App\Lib\Vcode;
 use App\Models\User;
 use App\Lib\Response;
+use App\Lib\PHP_COOKIE;
+
 class Login
 {
+
     public $middle = [
-    'index' => 'Check_login'
+    'GetUserinfo' => 'Check_login'
 
     ];
   /*错误代码 && 错误信息
@@ -24,9 +27,9 @@ class Login
    *
    * @param string $mail 邮箱地址
    * @param string $password  密码
-   * @return status.状态(609要求验证码) errmsg.错误信息 data.成员信息包括id,name,sex,photo,department,habbit,position,blog,phone,introduction
+   * @return status.状态(609要求验证码) errmsg.错误信息 data.成员信息包括id,nickname,mail,role,ip,photo
    */
-    public function CheckLogin( $mail,$password)
+    public function CheckLogin($mail,$password,$log)
     {
         if(Session::get("errer_num")==null)
         {
@@ -39,11 +42,31 @@ class Login
           {
               if(password_verify($password, $d[0]['password']))//判断密码
               {
+                 $get_password=$d[0]['password'];
                  unset($d[0]['password']); 
+                 unset($d[0]['overdue']); 
+                 unset($d[0]['token']); 
                  $d[0]['ip']=$_SERVER["REMOTE_ADDR"];
                  Session::set("user", $d[0]);//保存session
                  Session::set("errer_num", 0);// 成功后置为0
-                 Response::out(200,$d[0]);
+                 if($log==1) //判断是否保存登录
+                 {
+                    $time=date("y-m-d H:i:s",time()+60*60*24*7);//设置保存时间的一周
+                    if(isset($_COOKIE['token']))//如何存在token值
+                    {
+                         $statuss= User::where('token', '=',$_COOKIE['token'])->update(['overdue'=>$time]);//更新数据库的过期时间
+                         setcookie("token",$_COOKIE['token'], time()+3600+60*60*24*10,'/'); //保存cookie的时间为10天
+                     
+                    }
+                    else{//不存在token值
+                         $token=md5(rand(1000000,9999999).time());//设置token值
+                         $str= User::where('mail', '=',$d[0]['mail'])->update(['overdue'=>$time,'token'=>$token]);//更新token
+                         setcookie("token",$token, time()+3600+60*60*24*10,'/'); //设置token过期为10天
+                    }
+             
+                 }
+                 Response::out(200,$d[0]);//返回数据
+
               }
               else
               {
@@ -80,7 +103,23 @@ class Login
     public function logout()
     {
       $T= Session::remove("user", null);
+      if(isset($_COOKIE['token']))//清空token
+      {
+          $str= User::where('token', '=',$_COOKIE['token'])->update(['overdue'=>null,'token'=>null]);
+           setcookie('token','',time()-360000,"/");
+      }
       Response::out(200);
     }
+  /**官网——获取登录用户信息
+   *
+   * @return status.状态 errmsg.错误信息 data.成员信息包括id,nickname,mail,role,ip,photo
+   */
+    public function GetUserinfo()
+    {
+          Response::out(200,Session::get("user"));
+    }
+
+  
+
     
 }
--- App/Middles/check_login.php
+++ App/Middles/check_login.php
@@ -2,6 +2,13 @@
 namespace App\Middles;
 use App\Controllers\Session;
 use App\Lib\Response;
+use App\Models\Uer_session;
+use App\Models\User;
+
+
+
+
+
 class Check_login implements MiddleWare
 {
     public function before($request)
@@ -21,8 +28,49 @@ class Check_login implements MiddleWare
     	}
     	else
     	{
-    	   Response::out(300);
-    		return false;
+            if(isset($_COOKIE['token']))//判断存在token值
+            {
+                   $d=user::where("token","=",$_COOKIE['token'])->select();//寻找数据库与token值对应的数据
+                    if(sizeof($d)!=0)//找到
+                     {
+                             if(strtotime(date("y-m-d H:i:s",time()))<strtotime($d[0]['overdue']))//判断是否过期token
+                             {
+                                  $get_password=$d[0]['password'];
+                                  unset($d[0]['password']); 
+                                  unset($d[0]['overdue']); 
+                                  unset($d[0]['token']); 
+                                  $d[0]['ip']=$_SERVER["REMOTE_ADDR"];
+                                  Session::set("user", $d[0]);//保存session
+                                   if($d[0]['status']==1)
+                                   {
+                                         Response::out(300);
+                                         return false;
+                                   }
+                                   else{
+                                        return true;
+                                   }
+                                
+                             }
+                             else//过期后清空
+                             { 
+                                  $str= User::where('token', '=',$_COOKIE['token'])->update(['overdue'=>null,'token'=>null]);
+                                  setcookie('token','',time()-360000,"/");
+                                  Response::out(300);
+                                  return false;
+                             }
+            
+                         
+                     }
+                     else
+                     {
+                         Response::out(300);
+                        return false;
+                     }
+             }
+            else{
+    	       Response::out(300);
+    		  return false;
+            }
     	}
         
 	}
