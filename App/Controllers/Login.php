a3d0f3182321c15773b3e2d72eb864520450f9f4
linzizhaogif <aa837610059@2980.com>
2016-10-06 19:24:03 +0800

    no message

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Controllers;
+
 use  App\Lib\Vcode;
 use App\Models\User;
 use App\Lib\Response;
@@ -31,7 +32,7 @@ class Login
    * @param string $log  判断是否保存用户(1为保存,0为不保存)
    * @return status.状态(609要求验证码) errmsg.错误信息 data.成员信息包括id,nickname,mail,role,ip,photo
    */
-    public function CheckLogin($mail,$password,$log)
+    public function CheckLogin($mail,$password,$log=0)
     {
         if(Session::get("errer_num")==null)
         {
@@ -46,7 +47,7 @@ class Login
         $d=User::where('mail', '=', $mail)->select();//判断邮箱是否存在
         if(sizeof($d)!=0)//邮箱存在
         {
-          if($d[0]['status']==0)//判断用户是否被限制登录
+          if($d[0]'status']==0)//判断用户是否被限制登录
           {
               if(password_verify($password, $d[0]['password']))//判断密码
               {
@@ -55,25 +56,13 @@ class Login
                  unset($d[0]['overdue']); 
                  unset($d[0]['token']); 
                  $d[0]['ip']=$_SERVER["REMOTE_ADDR"];
-                 Session::set("user", $d[0]);//保存session
-                 Session::set("errer_num", 0);// 成功后置为0
-                 if($log==1) //判断是否保存登录
-                 {
-                    $time=date("y-m-d H:i:s",time()+60*60*24*7);//设置保存时间的一周
-                    if(isset($_COOKIE['token']))//如何存在token值
-                    {
-                         $statuss= User::where('token', '=',$_COOKIE['token'])->update(['overdue'=>$time]);//更新数据库的过期时间
-                         setcookie("token",$_COOKIE['token'], time()+3600+60*60*24*10,'/'); //保存cookie的时间为10天
-                     
-                    }
-                    else{//不存在token值
-                         $token=md5(rand(1000000,9999999).time());//设置token值
-                         $str= User::where('mail', '=',$d[0]['mail'])->update(['overdue'=>$time,'token'=>$token]);//更新token
-                         setcookie("token",$token, time()+3600+60*60*24*10,'/'); //设置token过期为10天
-                    }
-             
-                 }
-                 Response::out(200,$d[0]);//返回数据
+                 $data2=[
+                    'user'=>$d[0],
+                    'error_num'=>0
+                  ]
+                 $token=sha1($d[0]['nickname'].time().mt_rand(100,999));
+                 Cache::set($token,json_encode($data2),60*60*24*7);//保存session
+                 Response::out(200,$token);//返回数据
 
               }
               else
@@ -128,6 +117,27 @@ class Login
           Response::out(200,Session::get("user"));
     }
 
+
+  public function get()
+  {
+    Response(__ROOT__);
+  }
+  public function setdata()
+  {
+   //    $data=[
+   //      'name'=>'dsds',
+   //      'ps'=>'123456'
+
+   //    ];
+   // $data2=[
+   //        'user'=>$data,
+   //        'error_num'=>0
+   //        ];
+
+   //    Response(json_encode($data2));
+   // Cache::set('name','dsdsds');
+    //Response(Cache::get('name'));
+  }
   
 
     
